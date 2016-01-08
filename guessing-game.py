6878889d11635a842f35be8da1908753dfd916d5
Annie He <anniehe.yt@gmail.com>
2016-01-07 21:14:27 -0800

    generated random number

--- guessing-game.py
+++ guessing-game.py
@@ -1,9 +1,15 @@
+from random import randint
+
+random_number = randint(1, 100)
+
 print "Hello there! Welcome to the number guessing game!"
 
 name = raw_input("What's your name? ")
 
 print "%s, I'm thinking of a number between 1 and 100." % (name)
 print "Try to guess my number!"
+
+# print random_number
 
 # choose random number between 1 and 100
 # while True:
