f2eb3b2d3e98bc4809f9d2c7a413226a85621d50
Annie He <anniehe.yt@gmail.com>
2016-01-07 21:08:24 -0800

    greeting the player

---
+++ guessing-game.py
@@ -0,0 +1,11 @@
+print "Hello there! Welcome to the number guessing game!"
+
+# greet player
+# get player name
+# choose random number between 1 and 100
+# while True:
+#     get guess
+#     if guess is incorrect:
+#         give hint
+#     else:
+#         congratulate player
