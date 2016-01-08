ad1424683303e797889647a41cdb42410c7b32d1
Annie He <anniehe.yt@gmail.com>
2016-01-07 21:13:05 -0800

    giving instructions

--- guessing-game.py
+++ guessing-game.py
@@ -2,7 +2,9 @@ print "Hello there! Welcome to the number guessing game!"
 
 name = raw_input("What's your name? ")
 
-# get player name
+print "%s, I'm thinking of a number between 1 and 100." % (name)
+print "Try to guess my number!"
+
 # choose random number between 1 and 100
 # while True:
 #     get guess
