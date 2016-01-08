a5f455667f9289d87ba24bec7ff4db22fe8f76cc
Annie He <anniehe.yt@gmail.com>
2016-01-07 21:26:52 -0800

    added hints, game works

--- guessing-game.py
+++ guessing-game.py
@@ -4,21 +4,19 @@ random_number = randint(1, 100)
 guess = 0
 guess_count = 0
 
-print "Hello there! Welcome to the number guessing game!"
+print "Hello, there! Welcome to the number guessing game!"
 
 name = raw_input("What's your name? ")
 
 print "%s, I'm thinking of a number between 1 and 100." % (name)
 print "Try to guess my number!"
 
-# print random_number
-
 while guess != random_number:
     guess = int(raw_input("Your guess? "))
     guess_count += 1
-    # print guess_count
-
-#     if guess is incorrect:
-#         give hint
-#     else:
-#         congratulate player
+    if guess > random_number:
+        print "Your guess is too high, try again."
+    elif guess < random_number:
+        print "Your guess is too low, try again."
+    else:
+        print "Well done, %s! You found my number in %d tries!" % (name, guess_count)
