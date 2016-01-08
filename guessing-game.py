03c03a67419a7f7481941dcbda4fe9bc1e78368f
Annie He <anniehe.yt@gmail.com>
2016-01-07 21:44:47 -0800

    checks guess to stay within 1-100

--- guessing-game.py
+++ guessing-game.py
@@ -11,12 +11,18 @@ name = raw_input("What's your name? ")
 print "%s, I'm thinking of a number between 1 and 100." % (name)
 print "Try to guess my number!"
 
+# print random_number
+
 while guess != random_number:
     guess = int(raw_input("Your guess? "))
-    guess_count += 1
-    if guess > random_number:
-        print "Your guess is too high, try again."
-    elif guess < random_number:
-        print "Your guess is too low, try again."
+    if guess < 1 or guess > 100:
+        print "Your guess was not between 1-100. Try again."
     else:
-        print "Well done, %s! You found my number in %d tries!" % (name, guess_count)
+        guess_count += 1
+        print guess_count
+        if guess > random_number:
+            print "Your guess is too high, try again."
+        elif guess < random_number:
+            print "Your guess is too low, try again."
+        else:
+            print "Well done, %s! You found my number in %d tries!" % (name, guess_count)
